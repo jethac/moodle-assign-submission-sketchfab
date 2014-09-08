f6d3506db73f726d88a857ee31a48e46d62d690f
Jetha Chan <jethachan@gmail.com>
2014-09-08 14:49:49 +0800

    Basic lang strings.

--- lang/en/assignsubmission_sketchfab.php
+++ lang/en/assignsubmission_sketchfab.php
@@ -22,4 +22,8 @@
  * @copyright 2014 Jetha Chan
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+$string['default'] = 'Enabled by default';
+$string['default_help'] = 'If set, this submission method will be enabled by default for all new assignments.';
+$string['endpointurl'] = 'API endpoint';
+$string['endpointurl_help'] = 'The default API endpoint.';
 $string['pluginname'] = 'Sketchfab assignment submissions';
