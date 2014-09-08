cfabdf984f75e82d85cd362a8335c712f0ac9fce
Jetha Chan <jethachan@gmail.com>
2014-09-08 14:28:35 +0800

    Initial code commit; replaced GPLv2 with v3.

---
+++ sketchfab/locallib.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ 
+/**
+ * Sketchfab Assignment Submission Plugin
+ *
+ * @package    assignsubmission
+ * @subpackage sketchfab
+ * @copyright 2014 Jetha Chan
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+
+class assign_submission_sketchfab extends assign_submission_plugin {
+
+	/** 
+	 * 	Returns the name of this plugin.
+	 *
+	 *	@return string The name of this plugin.
+	 */
+	public function get_name() {
+		return get_string('pluginname', 'assignsubmission_sketchfab');
+	}
+
+
+    /**
+     * Add form elements for settings
+     *
+     * @param mixed $submission can be null
+     * @param MoodleQuickForm $mform
+     * @param stdClass $data
+     * @return true if elements were added to the form
+     */
+    public function get_form_elements($submission, MoodleQuickForm $mform, stdClass $data) {
+        global $CFG, $COURSE, $PAGE, $OUTPUT;
+
+        echo $OUTPUT->notification("Howdy doody!", "notifysuccess");
+    }
+
+}
---
+++ sketchfab/settings.php
@@ -0,0 +1,40 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Sketchfab Assignment Submission Plugin
+ *
+ * @package    assignsubmission
+ * @subpackage sketchfab
+ * @copyright 2014 Jetha Chan
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+// This should be off by default, I'm pretty sure.                                                     
+$settings->add(new admin_setting_configcheckbox('assignsubmission_sketchfab/default',                                                    
+                   new lang_string('default', 'assignsubmission_sketchfab'),                                                             
+                   new lang_string('default_help', 'assignsubmission_sketchfab'), 1));           
+
+$settings->add(
+	new admin_setting_configtext(
+		'assignsubmission_sketchfab/endpointurl',
+		new lang_string('endpointurl', 'assignsubmission_sketchfab'),
+		new lang_string('endpointurl_help', 'assignsubmission_sketchfab'),
+		'http://api.sketchfab.com/v2/models'
+	)
+)
